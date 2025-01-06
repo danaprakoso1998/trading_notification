935cb2b62c3d4ae347d99b0b31c2e5ac457db91e
* <Dana Prakoso Eka Saputra>
2025-01-06 17:36:29 +0700

    Last commit

---
+++ send_message.php
@@ -0,0 +1,30 @@
+<?php
+$message = $_POST['message'];
+$accessToken = file_get_contents("server_key.txt");
+data = [
+        'message' => [
+            'topic' => '/topics/all',
+            'notification' => [
+                'title' => "Hello world",
+                'body' => "This is a notification test",
+            ],
+            'data' => [
+            ]
+        ]
+    ];
+
+    // Initialize cURL
+    $ch = curl_init();
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $accessToken,
+        'Content-Type: application/json',
+    ]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    // Execute the request
+    $response = curl_exec($ch);
